5676fc582f4ef52aa4f95e34ccf40b9186751db3
Panagiotis Ioannidis <41004192+PanosId@users.noreply.github.com>
2018-08-27 23:20:09 +0300

    Update logfile.py

--- logfile.py
+++ logfile.py
@@ -1,29 +1,16 @@
 
 infile = r"C:\Users\paioa\Desktop\testlog.log"
 
-
 logs = []
-
 key_word = ["real time"]
 
- 
-
 with open(infile) as f:
-
     f = f.readlines()
 
- 
-
 for line in f:
-
     for word in key_word:
-
         if word in line:
-
             logs.append(line)
-
             break
-
- 
-
+       
 print(logs[-1])
